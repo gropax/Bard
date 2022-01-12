efa3d5bc03bfc4d1b35105935fddd29f9610ddb1
Maxime Laudrin <maxime.laudrin@protonmail.com>
2022-01-12 21:45:32 +0100

    NaivePhonologicalAligner : add unit tests for all vowel sounds.

--- test/Bard.Fra.Analysis.Tests/NaivePhonologicalAlignerTests.cs
+++ test/Bard.Fra.Analysis.Tests/NaivePhonologicalAlignerTests.cs
@@ -89,6 +89,7 @@ namespace Bard.Fra.Analysis.Tests
         // /ʒ/ sound
         [InlineData("géant", "g:Z é:e an:@ t:")]
         [InlineData("juri", "j:Z u:y r:R i:i")]
+        [InlineData("rangeai", "r:R an:§ ge:Z ai:e")]
 
         // /l/ sound
         [InlineData("livre", "l:l i:i v:v r:R e:")]
@@ -113,6 +114,102 @@ namespace Bard.Fra.Analysis.Tests
         // /ŋ/ sound
         [InlineData("parking", "p:p a:a r:R k:k i:i ng:G")]
 
+        // /i/ sound
+        [InlineData("ville", "v:v i:i ll:l e:")]
+        [InlineData("yves", "y:i v:v es:")]
+        [InlineData("naïf", "n:n a:a ï:i f:f")]
+        [InlineData("île", "î:i l:l e:")]
+
+        // /e/ sound
+        [InlineData("mangeai", "m:m an:§ ge:Z ai:e")]  // prononciation non standard
+        [InlineData("créées", "c:k r:R é:e é:e es:")]
+        [InlineData("aesophage", "ae:e s:z o:o ph:f a:a g:Z e:")]
+        [InlineData("æsophage", "ae:e s:z o:o ph:f a:a g:Z e:")]
+        [InlineData("oedipe", "oe:e d:d i:i p:p e:")]
+        [InlineData("œdipe", "œ:e d:d i:i p:p e:")]
+        [InlineData("aî", "aî:e")]  // ???
+        [InlineData("e", "e:e")] // ???
+
+        // /ɛ/ sound
+        [InlineData("mangeai", "m:m an:@ ge:j ai:E")]
+        [InlineData("affaîter", "a:a ff:f aî:E t:t e:e r:")]
+        [InlineData("abeille", "a:a b:b e:E ill:j e:")]
+        [InlineData("blette", "b:b l:l e:E tt:t e:")]
+        [InlineData("près", "p:p r:R è:E s:")]
+        [InlineData("fête", "f:f ê:E t:t e:")]
+        [InlineData("noël", "n:n o:o ë:E l:l")]
+        [InlineData("aéroport", "a:a é:E r:R o:o p:p o:O r:R t:")] // Prononciation non standard
+        [InlineData("aesophage", "ae:E s:z o:o ph:f a:a g:Z e:")] // Prononciation non standard
+        [InlineData("æsophage", "ae:E s:z o:o ph:f a:a g:Z e:")] // Prononciation non standard
+
+        // /a/ sound
+        [InlineData("aaron", "aa:a r:R on:§")]
+        [InlineData("vache", "v:v a:a ch:S e:")]
+        [InlineData("château", "ch:S â:a t:t eau:o")]
+
+        // /ɑ/ sound
+        [InlineData("gras", "g:g r:R a:A s:")]
+        [InlineData("pâtes", "p:p â:A t:t es:")]
+
+        // /ɔ/ sound
+        [InlineData("porc", "p:p o:O r:R c:")]
+
+        // /o/ sound
+        [InlineData("rot", "r:R o:o t:")]
+        [InlineData("chaud", "ch:S au:o d:")]
+        [InlineData("contrôle", "c:k on:§ t:t r:R ô:o l:l e:")]
+        [InlineData("bateau", "b:b a:a t:t eau:o")]
+
+        // /u/ sound
+        [InlineData("roue", "r:R ou:u e:")]
+        [InlineData("où", "où:u")]
+        [InlineData("août", "a:a oû:u t:")]
+        [InlineData("tutti", "t:t u:u tt:t i:i")]
+
+        // /y/ sound
+        [InlineData("bulle", "b:b u:y ll:l e:")]
+        [InlineData("mûre", "m:m û:y r:R e:")]
+        [InlineData("contigüe", "c:k on:§ t:t i:i g:g ü:y e:")]
+
+        // /ø/ sound
+        [InlineData("deux", "d:d eu:2 x:")]
+        [InlineData("oeufs", "oeu:2 fs:")]
+        [InlineData("œufs", "œu:2 fs:")]
+        [InlineData("oedipe", "oe:2 d:d i:i p:p e:")]  // Prononciation non standard
+        [InlineData("œdipe", "œ:2 d:d i:i p:p e:")]   // Prononciation non standard
+
+        // /œ/ sound
+        [InlineData("beurre", "b:b eu:9 rr:R e:")]
+        [InlineData("oeuf", "oeu:9 f:f")]
+        [InlineData("œuf", "œu:9 f:f")]
+        [InlineData("über", "ü:y b:b e:9 r:R")]
+        [InlineData("oedipe", "oe:9 d:d i:i p:p e:")]  // Prononciation non standard
+        [InlineData("œdipe", "œ:9 d:d i:i p:p e:")]   // Prononciation non standard
+
+        // /ə/ sound
+        [InlineData("lâchement", "l:l â:a ch:S e:° m:m en:@ t:")]
+
+        // /ɛ̃/ sound
+        [InlineData("alain", "a:a l:l ain:5")]
+        [InlineData("klein", "k:k l:l ein:5")]
+        [InlineData("brin", "b:b r:R in:5")]
+        [InlineData("chien", "ch:S i:j en:5")]
+
+        // /ɑ̃/ sound
+        [InlineData("taon", "t:t aon:@")]
+        [InlineData("flan", "f:f l:l an:@")]
+        [InlineData("chambre", "ch:S am:@ b:b r:R e:")]
+        [InlineData("enfermé", "en:@ f:f e:E r:R m:m é:e")]
+        [InlineData("camembert", "c:k a:a m:m em:@ b:b e:E r:R t:")]
+
+        // /ɔ̃/ sound
+        [InlineData("jambon", "j:Z am:@ b:b on:§")]
+        [InlineData("combe", "c:k om:§ b:b e:")]
+
+        // /œ̃/ sound
+        [InlineData("brun", "b:b r:R un:1")]
+        [InlineData("parfum", "p:p a:a r:R f:f um:1")]
+
         // Silent endings
         [InlineData("cornet", "c:k o:O r:R n:n e:E t:")]
         [InlineData("manger", "m:m an:@ g:Z e:e r:")]
@@ -129,10 +226,11 @@ namespace Bard.Fra.Analysis.Tests
         [InlineData("dehors", "d:d e:° h: o:O r:R s:")]        // 'h' silent inside
         [InlineData("poussah", "p:p ou:u ss:s a:a h:")]        // 'h' silent at the end
 
-        // Special digrams
+        // Special case
         [InlineData("sprezzatura", "s:s p:p r:R e:e zz:tz a:a t:t u:u r:R a:a")]
         [InlineData("poire", "p:p oi:wa r:R e:")]
         [InlineData("point", "p:p oin:w5 t:")]
+        [InlineData("maxime", "m:m a:a x:ks i:i m:m e:")]
         public void TestCompute(string graphemes, string rawAlignement)
         {
             var alignment = ParseAlignement(rawAlignement).ToArray();
@@ -227,7 +325,7 @@ namespace Bard.Fra.Analysis.Tests
             { 'O', "ɔ" }, { 'E', "ɛ" }, { '°', "ə" }, { '2', "ø" }, { '9', "œ" },
             { '5', "ɛ̃" }, { '1', "œ̃" }, { '@', "ɑ̃" }, { '§', "ɔ̃" }, { '8', "ɥ" },
             { 'S', "ʃ" }, { 'Z', "ʒ" }, { 'N', "ɲ" }, { 'R', "ʁ" }, { 'x', "χ" },
-            { 'G', "ŋ" },
+            { 'G', "ŋ" }, { 'A', "ɑ" }
         };
     }
 }
