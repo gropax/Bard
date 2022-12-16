21128484a16b17b854c97c86136839002e81788d
Maxime Laudrin <maxime.laudrin@protonmail.com>
2022-12-16 19:33:43 +0100

    Add input field in editor.

--- src/Bard.Fra.Web/ClientApp/src/app/pages/editor-page/editor-page.component.ts
+++ src/Bard.Fra.Web/ClientApp/src/app/pages/editor-page/editor-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-editor-page',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorPageComponent implements OnInit {
 
+  public text: string;
+  public textSubject = new BehaviorSubject<string>("");
+  public $text = this.textSubject.asObservable()
+    .pipe(debounceTime(500));
+
   constructor() { }
 
   ngOnInit(): void {
+    this.$text.subscribe(text => console.log(text));
+  }
+
+  public updateText() {
+    this.textSubject.next(this.text);
   }
 
 }
